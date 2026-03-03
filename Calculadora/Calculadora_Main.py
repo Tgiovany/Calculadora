fa57fc5850c1654dcb1efedb2e81e83b6fcd6632
Thomas Geovany <thomasgiovany@gmail.com>
2026-03-03 10:59:51 -0300

    B
    --INSERT --
    Primeiro commit da calculadora.

---
+++ Calculadora/Calculadora_Main.py
@@ -0,0 +1,9 @@
+#Construir uma calculadora com todas as operações aritméticas.
+
+bottom_plus = 0
+bottom_minus = 0
+bottom_equals = 0
+
+teste = input('Teste pararara: ')
+if bottom_plus:
+    print("Plus")
