1d3ed5d2434b0e2a252f63c69fc74c311b82b08c
Thomas Geovany <thomasgiovany@gmail.com>
2026-03-07 10:32:42 -0300

    Feat: Implementando interface Front-end

--- Calculadora/Calculadora_Main.py
+++ Calculadora/Calculadora_Main.py
@@ -1,14 +1,36 @@
 # Construir uma calculadora com todas as operações aritméticas.
+#Thomas - alteração gráfica dos widgets (label, btn e entry)
+#Gabriel - lógica
 
-from tkinter import *
 from tkinter import ttk
+import tkinter as tk
+from PIL import Image, ImageTk
+
+#background config
+root = tk.Tk()
+root.geometry ('470x300')
+root.title('Calculadora')
+image_path = 'Images/Base-white.png' #caminho do png (arquivo bruto).
+image_original = Image.open(image_path) #carrega os dados do png.
+image_tk = None
+
+def redimensionar_fundo(event):
+    global image_tk
+    image_tk = ImageTk.PhotoImage(image_original)
+
+imagem_tk = ImageTk.PhotoImage(image_original) #pega o bruto e converte para o formato TKinter.
+label_bg = tk.Label(root, image=imagem_tk, text='Calculadora', compound='center')
+label_bg.grid(row=0, column=0, )
+
+
+
+
 
 plus_btn = 0
 minus_btn = 0
-nultiplication_btn = 0
+multiplication_btn = 0
 equal_btn = 0
+
+
 clear_btn = False
 
-teste = input('Teste pararara: ')
-if plus_btn:
-    print("Plus")
